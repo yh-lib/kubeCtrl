12d893a8aa41ea7040d5f7a7d8d7022de37d8b9e
yaohui.li <liyaohuiisgreat@gmail.com>
2026-03-26 02:04:57 +0800

    后端：拆分clientSet实例化，创建namespace功能已完成

--- server/controllers/controllers.go
+++ server/controllers/controllers.go
@@ -1,2 +1,61 @@
 // 控制器层  实现路由的处理逻辑
 package controllers
+
+import (
+	"errors"
+	"server/config"
+	"server/utils/logs"
+
+	"github.com/gin-gonic/gin"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type BasicInfo struct {
+	ClusterId string `json:"clusterId" form:"clusterId"`
+	NameSpace string `json:"nameSpace" form:"nameSpace"`
+}
+
+func BasicInit(c *gin.Context) (clientSet *kubernetes.Clientset, basicInfo BasicInfo, err error) {
+	// 绑定前端传递的数据到后端变量中
+	basicInfo = BasicInfo{}
+	requestMethod := c.Request.Method
+	switch requestMethod {
+	case "GET":
+		err = c.ShouldBindQuery(&basicInfo)
+	case "POST":
+		err = c.ShouldBindJSON(&basicInfo)
+	default:
+		err = errors.New("不支持的请求类型")
+	}
+	if basicInfo.NameSpace == "" {
+		basicInfo.NameSpace = "default"
+	}
+	if err != nil {
+		msg := "请求数据绑定后端失败: " + err.Error()
+		return nil, basicInfo, errors.New(msg)
+	}
+	// 实例化clientSet
+	kubeConfig := config.ClusterKubeconfig[basicInfo.ClusterId]
+	kubeConfigByte := []byte(kubeConfig)
+	// kubeConfig
+	restConfig, err := clientcmd.RESTConfigFromKubeConfig(kubeConfigByte)
+	if err != nil {
+		msg := "实例化 kubeconfig 失败:" + err.Error()
+		return nil, basicInfo, errors.New(msg)
+	}
+	// clientSet
+	clientSet, err = kubernetes.NewForConfig(restConfig)
+	if err != nil {
+		msg := "实例化clientSet失败:" + err.Error()
+		return nil, basicInfo, errors.New(msg)
+	}
+	// 校验 kubeconfig
+	serverVersion, err := clientSet.Discovery().ServerVersion()
+	if err != nil {
+		msg := "kubeconfig 校验失败: " + err.Error()
+		return nil, basicInfo, errors.New(msg)
+	}
+	logs.Debug(nil, "集群版本："+serverVersion.String())
+	return clientSet, basicInfo, nil
+}
--- server/controllers/namespace/Create.go
+++ server/controllers/namespace/Create.go
@@ -1,11 +1,33 @@
 package namespace
 
 import (
+	"context"
+	"server/config"
+	"server/controllers"
 	"server/utils/logs"
 
 	"github.com/gin-gonic/gin"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func Create(c *gin.Context) {
 	logs.Info(nil, "创建逻辑")
+	returnData := config.ReturnData{}
+	clientSet, basicInfo, err := controllers.BasicInit(c)
+	var newNameSpace corev1.Namespace
+	newNameSpace.Name = basicInfo.NameSpace
+	if err != nil {
+		logs.Error(map[string]any{"Error": err}, "clientSet 初始化失败")
+	}
+	_, err = clientSet.CoreV1().Namespaces().Create(context.TODO(), &newNameSpace, metav1.CreateOptions{})
+	if err != nil {
+		returnData.Status = 400
+		returnData.Message = "创建 namespace: " + newNameSpace.Name + " 失败:" + err.Error()
+	} else {
+		returnData.Status = 200
+		returnData.Message = "创建 namespace: " + newNameSpace.Name + " 成功:"
+	}
+	logs.Error(map[string]any{"Error": err}, "创建 namespace: "+newNameSpace.Name+" 失败:")
+	c.JSON(200, returnData)
 }
