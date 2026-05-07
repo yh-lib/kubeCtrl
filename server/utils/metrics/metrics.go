5b1e78dcda44a6e4161fec2b5dc09ff3383b7df2
yaohui.li <liyaohuiisgreat@gmail.com>
2026-05-07 23:31:05 +0800

    后端 node metrics 接口已完成

--- server/utils/metrics/metrics.go
+++ server/utils/metrics/metrics.go
@@ -1,47 +1,66 @@
 package metrics
 
 import (
-	"fmt"
+	"context"
+	"time"
 
 	corev1 "k8s.io/api/core/v1"
-	typedv1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/metrics/pkg/client/clientset/versioned"
+	"k8s.io/metrics/pkg/client/clientset/versioned/typed/metrics/v1beta1"
 )
 
 // 定义kubeutils的接口
 type MetricsUtilser interface {
 	// namespace
-	metrics(namespace string) (any, error)
+	Metrics(namespace string) (any, error)
 }
 
 // 定义结构体
 type Node struct {
-	InstanceInterface typedv1.CoreV1Interface
+	InstanceInterface v1beta1.NodeMetricsInterface
 	Item              *corev1.Node
 }
 
-// var (
-// 	node corev1.Node
-// )
-
-func (c *Node) metrics() {
-	fmt.Println("执行metrics逻辑")
+type ResourceInstance struct {
+	Kubeconfig string
+	Clientset  *versioned.Clientset
 }
 
-// // New函数可以用于配置一些默认值
-// func NewNode(kubeconfig string, item *corev1.Node) *Node {
-// 	// 首先调用instance的init函数，生成一个ResourceInstance的实例，并配置默认值和生成clientset
-// 	instance := ResourceInstance{}
-// 	instance.Init(kubeconfig)
-// 	// 定义一个Node实例
-// 	resource := Node{}
-// 	resource.InstanceInterface = instance.Clientset.CoreV1()
-// 	resource.Item = item
-// 	return &resource
-// }
+func (c *Node) Metrics(namespace string) (items any, err error) {
+	list, err := c.InstanceInterface.List(context.TODO(), metav1.ListOptions{})
+	items = list.Items
+	return items, err
+}
 
-// func (c *Node) Create(namespace string) error {
-// 	logs.Info(map[string]interface{}{"名称": c.Item.Name}, "创建Node")
-// 	c.Item.ResourceVersion = ""
-// 	_, err := c.InstanceInterface.Nodes().Create(context.TODO(), c.Item, v1.CreateOptions{})
-// 	return err
-// }
+// 构造 Node 结构体
+func NewNode(kubeconfig string, item *corev1.Node) *Node {
+	// 构造ResourceInstance实例
+	instance := ResourceInstance{}
+	instance.Init(kubeconfig)
+	// 定义一个Node实例
+	resource := Node{}
+	resource.InstanceInterface = instance.Clientset.MetricsV1beta1().NodeMetricses()
+	resource.Item = item
+	return &resource
+}
+
+func (c *ResourceInstance) Init(kubeconfig string) {
+	// kubeconfig 赋值
+	c.Kubeconfig = kubeconfig
+	// 实例化 metrics clientSet
+	restConfig, err := clientcmd.RESTConfigFromKubeConfig([]byte(c.Kubeconfig))
+	if err != nil {
+		msg := "解析kubeconfig错误:" + err.Error()
+		panic(msg)
+	}
+	// 设置超时时间
+	restConfig.Timeout = 15 * time.Second
+	clientset, err := versioned.NewForConfig(restConfig)
+	if err != nil {
+		msg := "创建metrics clientset失败:" + err.Error()
+		panic(msg)
+	}
+	c.Clientset = clientset
+}
